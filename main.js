3e9381ed7b63245ccb94dc79a950d17ebf25e21e
miranda-huallpa <amirandah@fi.uba.ar>
2021-12-20 00:54:34 -0300

    Condicionales y listas

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ var app = new Vue ({
     data: {
         product: 'Socks',
         image: 'assets/vmSocks-green-onWhite.jpg',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2255,
+                variantColor: "blue"
+            }
+        ]
+        
     }
 })
